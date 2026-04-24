4e70edb147de24f3aa6798bf6f8b605fbb51cfdc
denzxje157 <minhminh887701@gmail.com>
2026-04-24 21:37:37 +0700

    update main

--- main.py
+++ main.py
@@ -3,17 +3,57 @@ import Data_loader
 import quiz_engine
 import logger
 
-en, vi = Data_loader.lay_du_lieu()
+list_tienganh, list_tiengviet = Data_loader.lay_du_lieu()
 
 print("--- CHUONG TRINH TRAC NGHIEM ---")
-chon = input("Chon che do (1: Viet-Anh, 2: Anh-Viet): ")
+print("1: Viet-Anh")
+print("2: Anh-Viet")
+chon = input("Chọn chế độ (1 hoặc 2): ")
 
 diem = 0
-cau_sai = []
+danh_sach_cau_sai = []
+
+cau_da_hoi = [] 
 
 for i in range(10):
-    dung = random.randint(0, len(en) - 1)
-    ds_lua_chon = quiz_engine.tao_bo_dap_an(dung, len(en))
+    vi_tri_dung = random.randint(0, len(list_tienganh) - 1)
+    while vi_tri_dung in cau_da_hoi:
+        vi_tri_dung = random.randint(0, len(list_tienganh) - 1)
     
-logger.ghi_ket_qua(diem, cau_sai)
-print("Da xong!")
+    cau_da_hoi.append(vi_tri_dung)
+
+    ba_dap_an = quiz_engine.tao_bo_dap_an(vi_tri_dung, len(list_tienganh))
+
+    print("\n--- Câu số", i + 1, "---")
+
+    if chon == '1':
+        print("Từ '" + list_tiengviet[vi_tri_dung] + "' có nghĩa Tiếng Anh là gì?")
+
+        for n in range(len(ba_dap_an)):
+            chi_so_dap_an = ba_dap_an[n]
+            print(str(n + 1) + ". " + list_tienganh[chi_so_dap_an])
+    else:
+        print("Tưd '" + list_tienganh[vi_tri_dung] + "' có nghĩa Tiếng Việt là gì?")
+        for n in range(len(ba_dap_an)):
+            chi_so_dap_an = ba_dap_an[n]
+            print(str(n + 1) + ". " + list_tiengviet[chi_so_dap_an])
+
+    cau_tra_loi = input("Nhập lựa chọn của bạn (1, 2, 3): ")
+
+    dap_an_dung_nam_o_so = 0
+    for n in range(len(ba_dap_an)):
+        if ba_dap_an[n] == vi_tri_dung:
+            dap_an_dung_nam_o_so = n + 1
+
+    if cau_tra_loi == str(dap_an_dung_nam_o_so):
+        print("=> Chính xác!")
+        diem = diem + 1 
+    else:
+        print("=> Sai rồi!")
+        tu_sai = list_tienganh[vi_tri_dung] + " - " + list_tiengviet[vi_tri_dung]
+        danh_sach_cau_sai.append(tu_sai)
+
+logger.ghi_ket_qua(diem, danh_sach_cau_sai)
+
+print("Đã xong, bạn đúng được", diem, "/ 10 câu.")
+print("Kiểm tra file ketqua.txt để xem chi tiết nha.")
